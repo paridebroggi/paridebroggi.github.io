f6095b91cfd0a325352ebf3b79dcc825bb3733c1
p <prdbrg@users.noreply.github.com>
2023-05-26 22:18:17 +0200

    Update google-analytics.js

--- _includes/google-analytics.js
+++ _includes/google-analytics.js
@@ -1,5 +1,3 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-SPKEHR0BRY"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
