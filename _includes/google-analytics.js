fc2ec69074f05cd689f4502407cff4ce30332ee7
p <prdbrg@users.noreply.github.com>
2023-05-26 22:03:35 +0200

    Updated to GA4

--- _includes/google-analytics.js
+++ _includes/google-analytics.js
@@ -1,4 +1,9 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-28546477-2');
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-SPKEHR0BRY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SPKEHR0BRY');
+</script>
