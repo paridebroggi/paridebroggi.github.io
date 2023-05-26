3219e4a4be5e0e4bb381ce1b9e745564514e12aa
p <prdbrg@users.noreply.github.com>
2023-05-26 22:22:10 +0200

    Update google-analytics.js

--- _includes/google-analytics.js
+++ _includes/google-analytics.js
@@ -1,7 +1,5 @@
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-SPKEHR0BRY');
 
-  gtag('config', 'G-SPKEHR0BRY');
-</script>
